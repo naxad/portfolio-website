de42c54a90d8c611294cc79c156506445ded00bf
Coedex <tsevastasos@gmail.com>
2025-06-11 10:41:03 +0300

    Fixed homepage styling, logo path, implemented darkmode prototype

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-$(function() {
+$(document).ready(function () {
+  // Load navbar and footer
   $("#navbar").load("navbar.html");
   $("#footer").load("footer.html");
+
+  // Apply saved dark mode
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    $('body').addClass('dark-mode');
+    $('#darkModeToggle').prop('checked', true);
+  }
+
+  // Toggle dark mode and save to localStorage
+  $('#darkModeToggle').on('change', function () {
+    if (this.checked) {
+      $('body').addClass('dark-mode');
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      $('body').removeClass('dark-mode');
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  });
 });
