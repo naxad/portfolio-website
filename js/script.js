7cb1b213783414078f96c42b5dd30d8ca8efcbf3
Coedex <tsevastasos@gmail.com>
2025-06-11 10:43:30 +0300

    Fixed dark mode toggle by moving logic inside navbar load callback

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 $(document).ready(function () {
-  // Load navbar and footer
-  $("#navbar").load("navbar.html");
-  $("#footer").load("footer.html");
+  // Load navbar and footer first
+  $("#navbar").load("navbar.html", function () {
+    // Now that navbar is loaded, we can safely bind to the toggle
+    const toggle = $('#darkModeToggle');
 
-  // Apply saved dark mode
-  if (localStorage.getItem('darkMode') === 'enabled') {
-    $('body').addClass('dark-mode');
-    $('#darkModeToggle').prop('checked', true);
-  }
-
-  // Toggle dark mode and save to localStorage
-  $('#darkModeToggle').on('change', function () {
-    if (this.checked) {
+    // Set initial state
+    if (localStorage.getItem('darkMode') === 'enabled') {
       $('body').addClass('dark-mode');
-      localStorage.setItem('darkMode', 'enabled');
-    } else {
-      $('body').removeClass('dark-mode');
-      localStorage.setItem('darkMode', 'disabled');
+      toggle.prop('checked', true);
     }
+
+    // On toggle click
+    toggle.on('change', function () {
+      if (this.checked) {
+        $('body').addClass('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+      } else {
+        $('body').removeClass('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+      }
+    });
   });
+
+  $("#footer").load("footer.html");
 });
